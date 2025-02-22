fa7d900993e7d452e3885aaaf51071c172cd74fc
AmanKumar636 <cs24m114@iittp.ac.in>
2025-02-22 11:58:15 +0530

    atabase Connected

--- server.js
+++ server.js
@@ -25,10 +25,13 @@ app.get("/", (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error(err));
+
 
 // Middleware to authenticate token (for users and hospitals)
 const authenticateToken = (req, res, next) => {
